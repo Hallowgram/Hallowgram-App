f9c000dfd223857d75d28494007d439caedaa1b6
Michael <mhaviv18@gmail.com>
2017-10-23 20:56:29 -0400

    added some js for profile page

---
+++ static/js/profile.js
@@ -0,0 +1,7 @@
+document.getElementById('edit-info').addEventListener('click', function(){
+	alert('clicked button!');
+	var name = document.getElementById('name-info');
+	var descrip = document.getElementById('descrip-info');
+
+	console.log(name);
+});
